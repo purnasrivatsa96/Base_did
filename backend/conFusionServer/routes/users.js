46489b89136aae7ba9f3ad73f41d98a44753eabb
Gautam Sharma <gautamsharma7234@gmail.com>
2022-04-07 10:21:29 -0400

    init

---
+++ backend/conFusionServer/routes/users.js
@@ -0,0 +1,112 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+var passport = require('passport');
+var authenticate = require('../authenticate');
+const cors = require('./cors');
+
+var router = express.Router();
+router.use(bodyParser.json());
+
+/* GET users listing. */
+router.get('/',cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      return next(err);
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content_type', 'application/json');
+      res.json(users);
+    }
+  })
+});
+
+router.post('/signup',cors.corsWithOptions,  (req, res, next) => {
+  console.log("sign up request");
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+    }
+    else {
+      if (req.body.firstname)
+        user.firstname = req.body.firstname;
+      if (req.body.lastname)
+        user.lastname = req.body.lastname;
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+          return ;
+        }
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'Registration Successful!'});
+        });
+      });
+    }
+  });
+});
+
+// router.post('/login',cors.corsWithOptions,  passport.authenticate('local'), (req, res) => {
+
+//   var token = authenticate.getToken({_id: req.user._id});
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'application/json');
+//   res.json({success: true, token: token, status: 'You are successfully logged in!'});
+// });
+
+
+
+
+router.post('/login', cors.corsWithOptions, (req, res, next) => {
+
+  passport.authenticate('local', (err, user, info) => {
+    if (err)
+      return next(err);
+
+    if (!user) {
+      res.statusCode = 401;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: false, status: 'Login Unsuccessful!', err: info});
+      return;
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        res.statusCode = 401;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: false, status: 'Login Unsuccessful!', err: 'Could not log in user!'});          
+        return next(err);;
+      }
+      
+      var token = authenticate.getToken({_id: req.user._id});
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      // res.render('index');
+      res.json({success: true, status: 'Login Successful!', token: token,user: user});
+    }); 
+  }) (req, res, next);
+});
+
+router.get('/logout', cors.corsWithOptions, function(req, res) {
+  req.logout();
+res.status(200).json({
+  status: 'Bye!'
+});
+});
+
+
+// router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+//   if (req.user) {
+//     var token = authenticate.getToken({_id: req.user._id});
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'application/json');
+//     res.json({success: true, token: token, status: 'You are successfully logged in!'});
+//   }
+// });
+
+module.exports = router;
